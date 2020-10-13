7dddc1f3ccb3b54c2d97d0813dc59fe40a96f8bf
nSedrickm <nsedrick101@gmail.com>
2020-10-13 16:07:31 +0100

    update templates

--- src/app/geo-api.service.spec.ts
+++ src/app/geo-api.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from './api.service';
+import { GeoApiService } from './geo-api.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
+describe('GeoApiService', () => {
+  let service: GeoApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    service = TestBed.inject(GeoApiService);
   });
 
   it('should be created', () => {
