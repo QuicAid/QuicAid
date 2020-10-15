65cdd9bf94c0d3a1b7bfb83193b869933db4604a
nSedrickm <nsedrick101@gmail.com>
2020-10-15 20:50:54 +0100

    add api-test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ResultsComponent } from './results/results.component';
 import { SymptomsComponent } from './symptoms/symptoms.component';
+import { ApiTestComponent } from './api-test/api-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ResultsComponent,
-    SymptomsComponent
+    SymptomsComponent,
+    ApiTestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
