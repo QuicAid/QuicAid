7fa0aa09bc97ba345e20b0e980627e71d9e9daba
nSedrickm <nsedrick101@gmail.com>
2020-10-15 20:00:51 +0100

    update diagnosis-api.service with HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ResultsComponent } from './results/results.component';
@@ -16,7 +17,8 @@ import { SymptomsComponent } from './symptoms/symptoms.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/diagnosis-api.service.ts
+++ src/app/diagnosis-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ConfigService } from './config.service';
-import {Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
 
 
@@ -9,148 +8,148 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class DiagnosisApiService {
-public classData:any;
-  constructor(public http: Http, public config: ConfigService) { }
+  public classData: any;
+  constructor(public http: HttpClient, public config: ConfigService) { }
 
-   loadSymptoms() {
+  loadSymptoms() {
 
-      let url = `${this.config.getAPIURL()}/symptoms`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/symptoms`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
   }
 
   loadIssues() {
 
-      let url = `${this.config.getAPIURL()}/issues`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/issues`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 
 
   loadIssueInfo(issueId) {
 
-      let url = `${this.config.getAPIURL()}/issues/${issueId}/info`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/issues/${issueId}/info`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
-
-  }
-
-  loadDiagnosis(selectedSymptoms,gender,yearOfBirth) {
-
-      let symptoms = selectedSymptoms.split(',');
-      let url = `${this.config.getAPIURL()}/diagnosis?symptoms=`+JSON.stringify(symptoms)+`&gender=${gender.value}&year_of_birth=${yearOfBirth}`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
-
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 
-  loadSpecialisations(selectedSymptoms,gender,yearOfBirth) {
+  loadDiagnosis(selectedSymptoms, gender, yearOfBirth) {
 
-      let symptoms = selectedSymptoms.split(',');
-      let url = `${this.config.getAPIURL()}/diagnosis/specialisations?symptoms=`+JSON.stringify(symptoms)+`&gender=${gender.value}&year_of_birth=${yearOfBirth}`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let symptoms = selectedSymptoms.split(',');
+    let url = `${this.config.getAPIURL()}/diagnosis?symptoms=` + JSON.stringify(symptoms) + `&gender=${gender.value}&year_of_birth=${yearOfBirth}`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 
-  loadProposedSymptoms(selectedSymptoms,gender,yearOfBirth) {
+  loadSpecialisations(selectedSymptoms, gender, yearOfBirth) {
 
-      let symptoms = selectedSymptoms.split(',');
-      let url = `${this.config.getAPIURL()}/symptoms/proposed?symptoms=`+JSON.stringify(symptoms)+`&gender=${gender.value}&year_of_birth=${yearOfBirth}`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let symptoms = selectedSymptoms.split(',');
+    let url = `${this.config.getAPIURL()}/diagnosis/specialisations?symptoms=` + JSON.stringify(symptoms) + `&gender=${gender.value}&year_of_birth=${yearOfBirth}`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
+
+  }
+
+  loadProposedSymptoms(selectedSymptoms, gender, yearOfBirth) {
+
+    let symptoms = selectedSymptoms.split(',');
+    let url = `${this.config.getAPIURL()}/symptoms/proposed?symptoms=` + JSON.stringify(symptoms) + `&gender=${gender.value}&year_of_birth=${yearOfBirth}`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
+
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 
   loadBodyLocations() {
 
-      let url = `${this.config.getAPIURL()}/body/locations`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/body/locations`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 
   loadBodySublocations(bodyLocationId) {
 
-      let url = `${this.config.getAPIURL()}/body/locations/${bodyLocationId}`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/body/locations/${bodyLocationId}`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 
-  loadBodySublocationSymptoms(bodySublocationId,selectorStatus) {
+  loadBodySublocationSymptoms(bodySublocationId, selectorStatus) {
 
-     let url = `${this.config.getAPIURL()}/symptoms/${bodySublocationId}/${selectorStatus.value}`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/symptoms/${bodySublocationId}/${selectorStatus.value}`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
   }
 
   loadRedFlagText(symptomId) {
 
-      let url = `${this.config.getAPIURL()}/redflag?symptomId=${symptomId}`;
-      let headers = new Headers()
-      let extraArgs = 'token='+this.config.getToken()+'&language='+this.config.getLanguage()+'&format='+this.config.getFormat();
-      url += url.indexOf("?") > 0 ? "&"+extraArgs : "?"+extraArgs;
+    let url = `${this.config.getAPIURL()}/redflag?symptomId=${symptomId}`;
+    let headers = new HttpHeaders()
+    let extraArgs = 'token=' + this.config.getToken() + '&language=' + this.config.getLanguage() + '&format=' + this.config.getFormat();
+    url += url.indexOf("?") > 0 ? "&" + extraArgs : "?" + extraArgs;
 
-      if(this.config.getFormat() == "json")
-          return this.http.get(url, {headers: headers}).pipe(map(data => { return data}))
-        else
-          return this.http.get(url, {headers : headers}).pipe(map((res:Response) =>   { return res}))
+    if (this.config.getFormat() == "json")
+      return this.http.get(url, { headers: headers }).pipe(map(data => { return data }))
+    else
+      return this.http.get(url, { headers: headers }).pipe(map((res: Response) => { return res }))
 
   }
 }
