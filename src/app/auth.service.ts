24c2db1291f3835de806709317750d25da5cda5d
nSedrickm <nsedrick101@gmail.com>
2020-10-18 21:43:15 +0100

    install crypto-js, update auth.service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private authURL: string = "https://sandbox-authservice.priaid.ch/login";
+  private apiKey: string = "";
+  private secretKey: string = "";
+  private hashed_credentials: string = "";
+  public authToken: string = ""
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    this.authToken = "";
+  }
+
+  getToken() {
+    let headers = new HttpHeaders().set("Authorization", "Bearer " + this.apiKey +":" + this.hashed_credentials);
+    return this.http.post(this.authURL, headers);
+  }
 }
